c9ffdc8e9289653373ef2521916525007b5edf62
Kalebe217 <101991515+Kalebe217@users.noreply.github.com>
2022-09-22 23:10:33 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -1,18 +1,18 @@
 function drop(ev){
   ev.preventDefault();
-  const pedraId_2 = ev.dataTransfer.getData("pedraId");
-  const nPedra = document.getElementById(pedraId_2)
-  if(nPedra.dataset.cor == 'red'
-  && npedra.parentElement.dataset.linha-1 == ev.target.dataset.linha
-  && (npedra.parentElement.dataset.coluna-1 == ev.target.dataset.coluna
-  || npedra.parentElement.dataset.coluna == ev.target.dataset.coluna)
-  || npedra.dataset.cor == 'black'
-  && npedra.parentElement.dataset.linha == ev.targent.dataset.linha-1
-  && (npedra.parentElement.dataset.coluna-1 == ev.targent.dataset.coluna
-  || npedra.parentElement.dataset.coluna == ev.targent.dataset.coluna-1)) {
-    npedra.parentElement.addEventListener('dragover',allowDrop)
-    ev.target.appendChild(npedra);
-    npedra.parentElement.removeElementListener('dragover', allowDrop)
+  const idPedra = ev.dataTransfer.getData('pedraId');
+  const pedra = document.getElementById(idPedra);
+  if(pedra.dataset.cor == 'red'
+  && pedra.parentElement.dataset.lin-1 == ev.target.dataset.lin
+  && (pedra.parentElement.dataset.col-1 == ev.target.dataset.col
+  || pedra.parentElement.dataset.col == ev.target.dataset.col-1)
+  || pedra.dataset.cor == 'black'
+  && pedra.parentElement.dataset.lin == ev.targent.dataset.lin-1
+  && (pedra.parentElement.dataset.col-1 == ev.targent.dataset.col
+  || pedra.parentElement.dataset.col == ev.targent.dataset.col-1)) {
+    pedra.parentElement.addEventListener('dragover', allowDrop)
+    ev.target.appendChild(pedra);
+    pedra.parentElement.removeElementListener('dragover', allowDrop)
     trocaVez()
     }
 }
@@ -20,12 +20,12 @@ function drop(ev){
 function trocaVez(){
   const allPawns = document.querySelectorAll('img');
   allPawns.forEach(pedra=>{
-    pedra.draggable = !pedra.draggable;
+    pedra.draggable = !pedra.draggable
   })
 }
 
 function drag(ev){
-  ev.dataTransfer.setData('pedraId', ev.target.id);
+  ev.dataTransfer.setData("pedraId", ev.target.id);
 }
 
 function allowDrop(ev){
@@ -38,10 +38,13 @@ function criaPedra(cor){
   img.setAttribute('widht',`${tamanhoCelula-4}px`);
   img.setAttribute('height',`${tamanhoCelula-4}px`);
   img.addEventListener('dragstart', drop);
-  img.id = `pedra ${pedraId++}`;
+  img.id = `p${pedraId++}`;
   img.dataset.cor = cor
   return img;
 }
+
+
+
 
 //declaração de variaveis iniciais
 const tamanhoCelula = 40;
@@ -52,7 +55,7 @@ document.body.append(tabuleiro())
 function tabuleiro(){
   const tab = document.createElement('table');
   tab.style.borderStyle = 'solid';
-  tab.style.Color = 'dark green';
+  tab.style.borderColor = 'dark green';
   tab.style.margin = 'auto';
   tab.style.borderSpacing = 0;
   for(let i = 0; i < 8 ; i++ ){
@@ -61,8 +64,8 @@ function tabuleiro(){
     for(let j = 0; j< 8 ; j++){
       const cel = document.createElement('td');
       line.append(cel);
-      cel.dataset.linha = i;
-      cel.dataset.coluna = j;
+      cel.dataset.lin = i;
+      cel.dataset.col = j;
       cel.style.width = `${tamanhoCelula}px`;
       cel.style.height =`${tamanhoCelula}px`;
       if(i%2 == j%2){
